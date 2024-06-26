e8fe9ce89d8307d1c0cbd917ce5261731aa766bb
bhautik <bdudhat293@gmail.com>
2024-06-26 12:00:39 +0530

    26-06

---
+++ Practice/26_06/26-06.js
@@ -0,0 +1,39 @@
+{
+
+    let a = 3
+    console.log(a)
+}
+
+{
+    const word = "hello world!"
+    console.log(word);
+}
+
+{
+    var a = 10
+    var b = 20
+    var c = 30
+
+    console.log(a, b, c)
+    console.log(a / b * c)
+    console.log(a - c)
+}
+
+{
+    let name_1 = "Amar "
+    let name_2 = "Akbar "
+    let name_3 = "Anthony "
+
+    console.log(name_1 + name_2 + name_3)
+}
+
+{
+    const Name_ = 'world'
+    console.log(Name_)
+}
+
+{
+
+    console.log(alert('hello owlrd'));
+}
+

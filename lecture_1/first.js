f9ccd11c975886a1083466b67b308563ac1e5740
bhautik <bdudhat293@gmail.com>
2024-06-27 10:19:19 +0530

    lecture_1

---
+++ lecture_1/first.js
@@ -0,0 +1,58 @@
+{
+
+    let a = 3
+    console.log(a)
+}
+
+{
+    const word = "hello world!"
+    console.log(word);
+}
+
+{
+    var a = 10
+    var b = 20
+    var c = 30
+
+    console.log(a, b, c)
+    console.log(a / b * c)
+    console.log(a - c)
+}
+
+{
+    let name_1 = "Amar "
+    let name_2 = "Akbar "
+    let name_3 = "Anthony "
+
+    console.log(name_1 + name_2 + name_3)
+}
+
+{
+    // consoel.log 
+    console.log("123")
+
+    // console.alert
+    console.warn("alert")
+
+    // document inner HTML
+
+    document.getElementById('p1').style = 'color:white;'
+    // document.getElementById('p1').style.backgroundColor = 'blue'
+
+    document.getElementById('b1').style.backgroundImage = 'url("../assets/elephant-2380009_1280.jpg")'
+
+}
+
+
+
+{
+    const Name_ = 'world'
+    console.log(Name_)
+}
+
+{
+
+    // alert('hello owlrd');
+    console.log("hello world")
+}
+

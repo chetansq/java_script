90cd3a8d42ffdb671ec60b344543d7ea5b2b23ba
chetan <chetan.sq05@gmail.com>
2024-06-28 10:04:18 +0530

    changes

---
+++ lecture_2/lec-2.js
@@ -0,0 +1,72 @@
+/* javascript hoisting */
+
+// var with hoisting
+{
+    // var a, b, c
+
+    // var a = 10
+    // var b = 20
+    // var c = 30
+
+    // console.log(a)
+    // console.log(b)
+    // console.log(c)
+}
+
+// let with hoisting
+{
+    // var x, y, z
+
+    // var x = 10
+    // var y = 20
+    // var z = 30
+
+    // console.log(x)
+    // console.log(y)
+    // console.log(z)
+}
+
+// const with hoisting
+{
+    // const a , b , c // Not possible hoisting
+    // const x = 10
+}
+
+/* javascript template literals (back-tik) */
+
+
+// let i = "skillqode"
+{
+    // let string = 'world'
+
+    //     // let string1 = "hello ${string}"
+    //     // let string1 = 'hello ${string}'
+    // let string1 = `hello ${string} , hello ${i}`
+
+    // console.log(string1);
+}
+
+{
+    // typeof 
+
+    // let a = 'abc'
+    // let b = 1
+    // let c = true
+
+    // console.log(typeof a, typeof b, typeof c)
+}
+
+/* javascript string define Method */
+
+// let skill = "js"
+// {
+// let hello1 = "I am ${skill} learning"
+
+// let hello1 = "I am \"js\" learning"
+// let hello2 = 'I am \'js\' learning'
+
+//     let hello3 = `I   am \` ${skill} \` learning`
+
+//     console.log(hello3)
+// }
+

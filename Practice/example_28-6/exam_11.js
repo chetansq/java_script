e926b0a53b3a3bddca486e3f49bad8f59207ea05
bhautik <bdudhat293@gmail.com>
2024-07-01 10:31:25 +0530

    example

---
+++ Practice/example_28-6/exam_11.js
@@ -0,0 +1,17 @@
+// Complex Assignments[2 types]
+
+{
+    let a = 10;
+    let b = 11;
+
+    let c = 1, d = 2, e = 3, f = 4;
+    c += b;
+    d -= a;
+    e /= c;
+    f *= e;
+
+    console.log(c);
+    console.log(d);
+    console.log(e);
+    console.log(f);
+}

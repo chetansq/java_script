92f20c7d06603adf0161813b038777fed0ed5671
bhautik <bdudhat293@gmail.com>
2024-07-03 11:59:00 +0530

    example

--- Practice/example_3-07/element.js
+++ Practice/example_3-07/element.js
@@ -1,2 +1,8 @@
 // Write a program that calculates the sum of all elements in an array
 
+let array = [10, 20, 30, 40, 50];
+let a = 0;
+for (let i = 0; i <= array[i]; i++) {
+    a += array[i];
+}
+console.log(a);
---
+++ Practice/example_3-07/largest.js
@@ -0,0 +1,17 @@
+//  Write a program that finds the largest number in an array
+
+{
+    let array = [2, 10, 5, 50, 100, 40, 1];
+
+    for (let i = 0; i <= array[i]; i++) {
+
+        if (array[i] < array[i + 1]) {
+
+            console.log(array[i])
+        }
+        else {
+            console.log("not")
+        }
+    }
+
+}

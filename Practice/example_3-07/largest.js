7ee506b90365152e2968ec9e3add095dcc40d597
bhautik <bdudhat293@gmail.com>
2024-07-04 10:11:20 +0530

    example

--- Practice/example_3-07/largest.js
+++ Practice/example_3-07/largest.js
@@ -1,17 +1,28 @@
 //  Write a program that finds the largest number in an array
 
 {
-    let array = [2, 10, 5, 50, 100, 40, 1];
+    // let array = [2, 10, 5, 50, 100, 40, 1];
 
-    for (let i = 0; i <= array[i]; i++) {
+    // for (let i in array) {
+    //     console.log(array[i]) // value of array on index number
+    //     console.log(i) //only index number
+    // }
 
-        if (array[i] < array[i + 1]) {
+    // for (let i of array) {
 
-            console.log(array[i])
-        }
-        else {
-            console.log("not")
+    //     console.log(array[i]) // array[i] is index number count, array[2] = 5 , array[2] is index number
+    //     // console.log(i) //print value
+    // }
+
+    let array = [2, 10, 5, 50, 100, 40, 10];
+    let a = -4;
+    for (let i of array) {
+
+        if (a < i) {
+            a = i;
         }
     }
+    console.log(a);
+
 
 }
---
+++ Practice/example_3-07/vowel.js
@@ -0,0 +1,12 @@
+// Write a program that counts the number of vowels in a given string
+
+{
+    let vowels = "javascript under";
+
+    for (let i of vowels) {
+        if (i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u') {
+            console.log(i);
+        }
+    }
+
+}

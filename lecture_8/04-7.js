62ac36e58b62d40a1edfc6fa8540f9073ddb2c76
darshan <skillqodedarshan@gmail.com>
2024-07-08 11:53:04 +0530

    lecture_8 complete

--- lecture_8/04-7.js
+++ lecture_8/04-7.js
@@ -144,19 +144,83 @@
 // charCodeAt(index)
 
 {
-    let str = '   This is javascript String Methods!   '
-    //         1234567890123456789012345678901234567890
+    // charAt(index)
+
+    // let str = '   This is javascript String Methods!   '
+    // //         1234567890123456789012345678901234567890
 
 
-    let char1 = str.charAt(-11);
-    console.log(char1);
-    let char2 = str.charAt(3);
-    console.log(char2);
-    let char3 = str.charAt(5);
-    console.log(char3);
-    let char4 = str.charAt(25);
-    console.log(char4);
-    let char5 = str.charAt(30);
-    console.log(char5);
+    // let char1 = str.charAt(-11);
+    // console.log(char1);
+    // let char2 = str.charAt(3);
+    // console.log(char2);
+    // let char3 = str.charAt(5);
+    // console.log(char3);
+    // let char4 = str.charAt(25);
+    // console.log(char4);
+    // let char5 = str.charAt(30);
+    // console.log(char5);
+
+
+    // charCodeAt(index)  return integer between 0 to 65535 ,it's represent return ascii code
+
+    // let str = '   This is javascript String Methods!   '
+    // //         1234567890123456789012345678901234567890
+
+
+    // let char1 = str.charCodeAt(-11);
+    // console.log(char1);
+    // let char2 = str.charCodeAt(3);
+    // console.log(char2);
+    // let char3 = str.charCodeAt(5);
+    // console.log(char3);
+    // let char4 = str.charCodeAt(25);
+    // console.log(char4);
+    // let char5 = str.charCodeAt(30);
+    // console.log(char5);
+
+}
+
+// javascript string.at() 
+// 
+{
+    // let str = 'hello javascript!'
+    // //         01234567890123456 
+
+    // let at1 = str.at(-10);
+    // let at2 = str.at(0);
+    // let at3 = str.at(-1);
+    // let at4 = str.at(4);
+
+    // console.log(at1);
+    // console.log(at2);
+    // console.log(at3);
+    // console.log(at4);
+}
+
+/* javascript string.includes() Method */
+
+// the includes() method of string values perform a case-sensitive
+
+// includes(searchString)
+// includes(searchString, position)
+
+{
+    // let str = 'javasript string include!'
+    //         0123456789012345678901234
+
+    // let include1 = str.includes('s');
+
+    // console.log(include1);
+
+    // let include2 = str.includes('s',11);
+    // let include3 = str.includes('a',4);
+    // let include4 = str.includes(' ',16);
+    // let include5 = str.includes('!',24);
+
+    // console.log(include2);
+    // console.log(include3);
+    // console.log(include4);
+    // console.log(include5);
 
 }

a09dc25a3135359c4257d6c93034cd0ff28aa272
darshan <skillqodedarshan@gmail.com>
2024-07-10 11:54:07 +0530

    get set methods complete

--- lecture_11/get.js
+++ lecture_11/get.js
@@ -1,5 +1,58 @@
 // date Methods
 
-let date = new Date();
-let 
-console.log(date);
+// let date = new Date();
+//Wed Jul 10 2024 11:21:51 GMT+0530 (India Standard Time)
+
+// let date = new Date("2002-12-05"); // show date
+
+// let date = new Date("1970-1-01 10:20:30"); // show date with time
+
+// let date = new Date(1970,1,1,10,20,30);
+
+// let date = new Date(1234464552144);
+
+// console.log(date);
+
+
+// let date = new Date();
+
+// console.log(date);  // same
+// console.log(date.toString()); // same
+// console.log(date.toLocaleString());  // 7/10/2024, 11:24:53 AM
+// console.log(date.toUTCString());  // Wed, 10 Jul 2024 05:55:27 GMT
+// console.log(date.toISOString());  // 2024-07-10T05:56:09.837Z
+
+// console.log(date.toDateString());  // Wed Jul 10 2024
+// console.log(date.toTimeString());  // 11:26:56 GMT+0530 (India Standard Time)
+
+
+// Get Methods
+
+// let getMethods = new Date();
+
+// console.log(getMethods);
+// console.log(getMethods.getFullYear()); // 2024
+// console.log(getMethods.getMonth()); // 6 / month start at 0 to 11 / 6 = jul
+// console.log(getMethods.getDate()); // 10  
+// console.log(getMethods.getDay()); // 3 / day start at 0 to 6 / 3 = wed  
+// console.log(getMethods.getHours()); // 11  
+// console.log(getMethods.getMinutes()); //  39  
+// console.log(getMethods.getMilliseconds()); // 456   
+// console.log(getMethods.getTime()); //  2423434235 , conver into milliseconds   
+
+// Set Methods
+
+let SetMethods = new Date();
+
+// console.log(SetMethods);
+
+// console.log(SetMethods.setDate(14));
+// SetMethods.setFullYear(2030);
+// SetMethods.setMonth(10);
+// SetMethods.setDate(13);
+// SetMethods.setHours(21);
+// SetMethods.setMinutes(21);
+// SetMethods.setSeconds(21);
+// SetMethods.setMilliseconds(21);
+// SetMethods.setTime();
+// console.log(SetMethods);

33d72f3cad3b508fd053eb0059f883ad93889e2f
darshan <skillqodedarshan@gmail.com>
2024-07-11 11:10:26 +0530

    changes

---
+++ lecture_10/method.js
@@ -0,0 +1,49 @@
+// Number Methods
+
+{
+    // let  a = '500';
+
+    // console.log((a)) // string 
+    // console.log((+a)) // number
+}
+
+
+{
+    let a = 100;
+
+    // console.log(a.toString());
+    // console.log(a.toExponential()); // 10 = 1e+1, 100 = 1e+2, 1000 = 1e+3, ..
+    // console.log(a.toExponential(1)); // 100 = 1.0e+2, 
+    // console.log(a.toExponential(3)); // 100 = 1.000e+2, 
+    // console.log(a.toPrecision()); // 100  
+    // console.log(a.toPrecision(4)); // 100.0 , toPrecision() Method is total digit count   
+    // console.log(a.toPrecision(2)); // 1.0e+2    
+    // console.log(a.toPrecision(5)); // 100.00    
+    // console.log(a.toFixed()); // 100    
+    // console.log(a.toFixed(3)); // 100.000 , this methods is add ZERO after digits(100) = 100.000   
+    // console.log(a.toFixed(2)); // 100.00    
+    // console.log(a.valueOf()); // it's a numeric 
+
+
+    
+
+
+}
+
+{
+
+    // console.log(parseInt("10")); // it's a numeric, output : 10 
+    // console.log(parseInt("10.33")); // it's a numeric output : 10
+    // console.log(parseInt(" 10 ")); // IGNORE SPACE = 10  
+    // console.log(parseInt("10yes  ")); // 10, nubmer before a string  
+    // console.log(parseInt("yes10 ")); // NaN, string before a number  
+    // console.log(parseInt("yes 10 ")); // NaN, string before a number  
+    // console.log(parseFloat("10.2324")); // 10.2324  
+    // console.log(parseFloat("yes 10")); // NaN  
+}
+
+
+{
+    // console.log(Number.isInteger(1234567890123456)); // true
+    // console.log(Number.isSafeInteger(123454)); // true
+}

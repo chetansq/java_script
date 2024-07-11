93a71ce8d97130be179043d2233e8ff611c76104
darshan <skillqodedarshan@gmail.com>
2024-07-11 11:59:38 +0530

    example..

---
+++ Practice/fuction_Example/func.js
@@ -0,0 +1,90 @@
+// 1. Write a program to convert temperatures between Celsius and Fahrenheit.
+
+{
+    //     function convert(c) {
+    //         let Fahrenheit = (c * 9 / 5) + 32
+    //         return Fahrenheit;
+
+    //     }
+    //     let a = (convert(32))
+
+    //     console.log(a);
+}
+
+
+
+
+// 2. Write a program that acts as a simple calculator performing addition, subtraction, multiplication, and division.
+{
+    // (
+    //     function (a, b) {
+    //         console.log(a + b);
+    //     }
+    // )(10, 20);
+
+    // (
+    //     function (a, b) {
+    //         console.log(a - b);
+    //     }
+    // )(10, 20);
+    // (
+    //     function (a, b) {
+    //         console.log(a * b);
+    //     }
+    // )(10, 20);
+    // (
+    //     function (a, b) {
+    //         console.log(a / b);
+    //     }
+    // )(10, 20);
+
+}
+
+// 3. Write a program to find the largest of three numbers.
+
+{
+    // function largest(a, b, c) {
+    //     if (a > b || a > c) {
+    //         return a;
+    //     }
+    //     else if (b > c) {
+    //         return b;
+    //     }
+    //     else {
+    //         return c;
+    //     }
+    // }
+
+    // let a = largest(10, 400, 50);
+    // console.log(a);
+}
+
+// 4. Write a program to find the factorial of a given number.
+
+{
+    // function factorial(a) {
+    //     let b = 1;
+    //     for (let i = 1; i <= a; i++) {
+
+    //         b *= i;
+
+    //     }
+    //     console.log(b);
+
+    // }
+
+    // let a = prompt("enter ther number..");
+    // factorial(a);
+}
+
+// 5. Write a program to print the first n terms of the Fibonacci sequence.
+
+{
+
+}
+
+// 6. Write a program to reverse a given string.
+
+{
+
+}

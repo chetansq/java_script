3bb815c0d676caecfac5f7d8f53e5ad525e7f409
bhautik <bdudhat293@gmail.com>
2024-07-12 18:01:05 +0530

    function 11 complete

--- Practice/function_exam_2/func.js
+++ Practice/function_exam_2/func.js
@@ -26,18 +26,72 @@
 // Expected Output : 'abeemrstw'
 // Assume punctuation and numbers symbols are not included in the passed string.
 
-// {
-//     function letters(alpha) {
-//         // let a = alpha.split();
-//         // console.log(a);
-//         // let b = "";
+{
+    // function letters(alpha) {
+    //     let a = alpha.split("");
 
-//         // for (let i =a.len)
-//         // if (a >= 'a' && a <= 'z') {
-//         //         b += a;
-//         //     }
-//         // console.log(b);
-//     }
+    //     // let b = a.sort().join("");
+    //     let b = a.sort();
+    //     let c = "";
+    //     for (i = 0; i <= b.length - 1; i++) {
+    //         if (i == ',') {
+    //             continue;
+    //         }
+    //         c += b[i];
 
-//     letters('alpha');
-// }
+    //     }
+    //     // let c = b.toString();
+    //     console.log(c);
+
+    // }
+
+    // let string = prompt("Enter a string");
+
+    // letters(string);
+}
+
+// 3. Write a JS function that accepts a string as a parameter and
+// converts the first letter of each word of the string in upper case.
+// Example string : 'the quick brown fox'
+// Expected Output : 'The Quick Brown Fox'
+
+
+{
+    // function uppercase(letters) {
+
+    //     let letter = letters.charAt().toUpperCase().slice(0) + letters.slice(1);
+    //     console.log(letter);
+
+    // }
+
+
+    // let letters = prompt("Enter a string ");
+    // uppercase(letters);
+
+
+    // function uppercase(letters) {
+
+    //     let letter = letters.split(" ");
+    //     for (let i = 0; i < letter.length; i++) {
+    //         letter[i] = letter[i][0].toUpperCase() + letter[i].substring(1);
+
+    //     }
+
+    //     // console.log(letter);
+    //     console.log(letter.join(" "));
+    // }
+
+
+    // let letters = prompt("Enter a string ");
+    // uppercase(letters);
+}
+
+// 4. Write a JavaScript function that accepts a string
+// as a parameter and finds the longest word within the string.
+// Example string : 'Web Development Tutorial'
+// Expected Output : 'Development'
+
+
+{
+    
+}

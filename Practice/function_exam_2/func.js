607ab19e3ea4b9dff9b1b124875e159e01f1995b
darshan <skillqodedarshan@gmail.com>
2024-07-12 12:03:27 +0530

    function example continue..

---
+++ Practice/function_exam_2/func.js
@@ -0,0 +1,43 @@
+// 1. Write a JS function that reverse a number. 
+// Example x = 15438;
+// Expected Output : 83451
+
+{
+    // function reverse_num(a) {
+
+    //     let b = "";
+
+    //     a = a.toString();
+    //     console.log(a);
+    //     // console.log(typeof a);
+
+    //     for (let i = a.length - 1; i >= 0; i--) {
+    //         b += a[i];
+    //     }
+
+    //     console.log(b);
+    // }
+
+    // reverse_num(3554);
+}
+
+// 2. Write a JS function that returns a passed string with letters in alphabetical order.
+// Example string : 'webmaster'
+// Expected Output : 'abeemrstw'
+// Assume punctuation and numbers symbols are not included in the passed string.
+
+// {
+//     function letters(alpha) {
+//         // let a = alpha.split();
+//         // console.log(a);
+//         // let b = "";
+
+//         // for (let i =a.len)
+//         // if (a >= 'a' && a <= 'z') {
+//         //         b += a;
+//         //     }
+//         // console.log(b);
+//     }
+
+//     letters('alpha');
+// }

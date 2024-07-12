402d485a36486ee3ae3ad15433e34144554ab405
bhautik <bdudhat293@gmail.com>
2024-07-12 10:00:21 +0530

    function example

--- Practice/fuction_Example/func.js
+++ Practice/fuction_Example/func.js
@@ -80,11 +80,112 @@
 // 5. Write a program to print the first n terms of the Fibonacci sequence.
 
 {
+    // function fibomnacci(a, b) {
 
+    //     let c;
+
+    //     let d = prompt("Enter Number .. ");
+
+    //     for (let i = 0; i <= d; i++) {
+
+    //         console.log(a);
+    //         c = a;
+    //         a += b;
+    //         b = c;
+    //     }
+    // }
+
+    // fibomnacci(0, 1);
 }
 
 // 6. Write a program to reverse a given string.
 
 {
+    // function reverse(str) {
+
+    //     let a = "";
+    //     // console.log(typeof (a));
+
+    //     for (let i = str.length - 1; i >= 0; i--) {
+    //         a += str[i];
+    //     }
+
+    //     console.log(a);
+    // }
+
+    // let str = prompt("enter a string");
+    // reverse(str);
+}
+
+
+// 7. Write a program to check if a string is a palindrome.
+
+{
+    // function palindrome(str) {
+
+    //     let a = "";
+    //     // console.log(typeof (a));
+
+    //     for (let i = str.length - 1; i >= 0; i--) {
+    //         a += str[i];
+    //     }
+
+    //     if (a == str) {
+    //         console.log("YES");
+
+    //     } else {
+    //         console.log("NO");
+
+    //     }
+
+
+    // }
+
+    // let str = prompt("enter a string");
+    // palindrome(str);
+}
+
+// 8. Write a program to find the sum of the first n natural numbers.
+
+{
+    // function natural(num) {
+    //     let sum = 0;
+
+    //     for (let i = 1; i <= num; i++) {
+    //         sum += i;
+    //     }
+
+    //     console.log(sum);
+    // }
+
+    // let num = prompt("Enter the number");
+
+    // natural(num);
+}
+
+// 9. Write a simple guessing game where the user has to guess a number between 1 and 10.
+
+{
+
+
+    // function random_num(num) {
+
+    //     let a = Math.floor(Math.random() * 100);
+    //     console.log(a);
+
+    //     if (a == num) {
+    //         return true;
+    //     }
+    //     else {
+    //         return false;
+    //     }
+    // }
+
+    // let num = prompt("enter a number ");
+
+    // let number = random_num(num);
+
+    // console.log(number);
+
 
 }

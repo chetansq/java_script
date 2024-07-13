b87b4030e4979ffc81422ec56cfa75a8b2bd1174
darshan <skillqodedarshan@gmail.com>
2024-07-13 11:15:51 +0530

    function lecture_12

---
+++ lecture_12/function.js
@@ -0,0 +1,58 @@
+
+//  function 
+// sum(); // it is print
+{
+
+    // function sum() {
+    //     console.log("the world is my territory");
+
+    // }
+
+    // sum(); // function call
+
+
+}
+
+{
+    //     function sum(a, b, c) {
+    //         console.log(a + b + c);
+    //     }
+
+    // sum(10, 2, 30); // 42
+    // sum(10, 2, 30.44); // 42.44
+    // sum(10, 2,'aF'); // 12aF
+}
+
+// constructor
+
+{
+    // let data = new Function("a", "b", 'return (a+b)'); // one line function
+    // let data = new Function("a", "b", 'return a'); // one line function
+    // let data = new Function("a", "b", 'return b'); // one line function
+
+    // console.log(data(4, 5));
+}
+
+// IIFE
+
+{
+
+    // (
+    //     function word(a, b) {
+    //         console.log(a + b);
+    //     })(1, 3);
+}
+
+// function as a variable (anonymous function )
+{
+
+    // let a = function () { } // one line function
+    // a();
+}
+
+// arrow function
+
+{
+    // let word = (a, b, c) => a - b * c;
+    // console.log(word(104, 5, 6));
+}

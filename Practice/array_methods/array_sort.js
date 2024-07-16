5a11ea84a5574298804c4483846b20e53c1b6dc3
darshan <skillqodedarshan@gmail.com>
2024-07-16 10:15:56 +0530

    changes

--- Practice/array_methods/array_sort.js
+++ Practice/array_methods/array_sort.js
@@ -3,82 +3,76 @@
     let data = [];
     let num;
 
-    // if (data == "") {
-    //     alert("data is empty..");
-    //     push_();
-    // } else {
 
+    function push_() {
+        if (data == "") {
 
+            num = prompt("Enter a data ..");
+            data.push(num);
+            // console.log(data);
+            document.getElementById("arr").innerHTML = data;
 
-        function push_() {
-            if (data == "") {
+        } else {
+            num = prompt("Enter a data ..");
+            data.push(num);
+            // console.log(data);
+            document.getElementById("arr").innerHTML = data;
 
-                num = prompt("Enter a data ..");
-                data.push(num);
-                // console.log(data);
-                document.getElementById("arr").innerHTML = data;
-
-            } else {
-                num = prompt("Enter a data ..");
-                data.push(num);
-                // console.log(data);
-                document.getElementById("arr").innerHTML = data;
-
-            }
         }
+    }
 
-        function pop_() {
+    function pop_() {
+        if (data == "") {
+
+            alert("array is empty..");
+
+        } else {
+            data.pop();
+            document.getElementById("arr").innerHTML = data;
+            console.log(data);
+        }
+    }
+
+    function unshift_() {
+        if (data == " ") {
+
+            num = prompt("Enter a data ..");
+            data.unshift(num);
+            document.getElementById("arr").innerHTML = data;
+            console.log(data);
+
+        } else {
+            num = prompt("Enter a data ..");
+            data.unshift(num);
+            document.getElementById("arr").innerHTML = data;
+            console.log(data);
+
+        }
+    }
+    function shift_() {
+        if (data == "") {
+
+            alert("array is empty..");
+
+        } else {
+            data.shift();
+            document.getElementById("arr").innerHTML = data;
+            console.log(data);
+        }
+    }
+
+    {
+        function sort_() {
             if (data == "") {
 
                 alert("array is empty..");
 
             } else {
-                data.pop();
+                console.log(data.sort((a, b) => a - b));
                 document.getElementById("arr").innerHTML = data;
                 console.log(data);
-            }
-        }
-
-        function unshift_() {
-            if (data == " ") {
-
-                num = prompt("Enter a data ..");
-                data.unshift(num);
-                document.getElementById("arr").innerHTML = data;
-                console.log(data);
-
-            } else {
-                num = prompt("Enter a data ..");
-                data.unshift(num);
-                document.getElementById("arr").innerHTML = data;
-                console.log(data);
-
-            }
-        }
-        function shift_() {
-            if (data == "") {
-
-                alert("array is empty..");
-
-            } else {
-                data.shift();
-                document.getElementById("arr").innerHTML = data;
-                console.log(data);
-            }
-        }
-
-        {
-            function sort_() {
-                if (data == "") {
-
-                    alert("array is empty..");
-
-                } else {
-                    console.log(data.sort((a, b) => a - b));
-                    document.getElementById("arr").innerHTML = data;
-                    console.log(data);
-                }
             }
         }
     }
+}
 

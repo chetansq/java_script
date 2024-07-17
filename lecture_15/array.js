745f96f17384c7b79d507fd49440c9a434b7d455
darshan <skillqodedarshan@gmail.com>
2024-07-17 12:00:52 +0530

    array methods

---
+++ lecture_15/array.js
@@ -0,0 +1,74 @@
+
+// falt() Method --> The flat() method concatenates sub-array elements.
+{
+
+    // let data = [10, 20, 30, 40, 50, 60, [1, 2, 3, 4, 5]];
+
+    // console.log(data);
+    // console.log(data.flat());
+
+}
+
+// foreach() method --> The forEach() method calls a function (a callback function) once for each array elements
+
+{
+    // let data = [10, 20, 30, 40, 50, 60, 70];
+
+    // let sum = 0;
+    // data.forEach((ele) => {
+    //     sum += ele;
+    //     // sum += (ele/10);
+    // });
+
+    // console.log(sum);
+}
+
+
+// map() method -->
+
+// The map() method creates a new array by performing a function on each array element.
+// The map() method does not execute the function for array elements without values.
+// The map() method does not change the original array.
+
+
+{
+    // let data = [10, 20, 30, 40, 50, 60, 70];
+
+    // let newarray = data.map((ele) => {
+    //     // return ele + 2;
+    //     // return ele / 2;
+    //     return ele * 2;
+    // })
+
+    // console.log(data); // origianl array 
+    // console.log(newarray); // increment by 2 in all array element
+
+
+}
+
+// filter() method -->
+// The filter() method creates a new array with array elements that pass a test.
+
+{
+    // let data = [10, 20, 90, 30, 40, 50, 60, 70];
+
+    // // data = data.filter((ele) => ele <= 30);
+    // // data = data.filter((ele) => ele < 300);
+    // // data = data.filter((ele) => ele == 30);
+
+    // console.log(data);
+}
+
+
+// find() method -->
+// The find() method returns the first element in the provided array that satisfies the provided testing function.If no values satisfy the testing function, undefined is returned
+
+{
+    // let data = [10, 20, 90, 30, 40, 50, 60, 70];
+
+    // data = data.find((ele) => ele <= 30);
+    // data = data.find((ele) => ele < 30);
+    // data = data.find((ele) => ele == 30); 
+
+    // console.log(data);
+}
